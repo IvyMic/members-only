2d4d444abf3d6267fefd42d8311872a843ef3c96
IvyMic <ibrahim_e@hotmail.co.uk>
2018-02-19 18:19:57 +0000

    set up login/log out system

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,28 @@
+module SessionsHelper
+
+  def log_in(user)
+    the_remember_token = user.remember_token
+    cookies.permanent[:the_remember_token] = the_remember_token
+    user.update_attribute(:remember_token, User.digest(the_remember_token))
+    self.current_user = user
+  end
+
+  def current_user
+    the_remember_token = User.digest(cookies[:the_remember_token])
+    user = @current_user ||= User.find_by(remember_token: the_remember_token)
+    return nil unless user
+    user
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def log_out
+    current_user.update_attribute(:remember_token, current_user.create_token_for_user)
+
+    cookies.delete(:the_remember_token)
+    self.current_user = nil
+  end
+
+end
